109ddfe12774d94ff378979aeb350ee73e7a88ce
juanan27 <148797929+juanan27@users.noreply.github.com>
2023-11-16 19:26:28 +0100

    Update complex_ftc.lean

--- Cauchy/lemmas/complex_ftc.lean
+++ Cauchy/lemmas/complex_ftc.lean
@@ -20,13 +20,15 @@ import Mathlib.MeasureTheory.Integral.SetIntegral
 -- Maybe there are more imports than what we actually need for this lemma, but it doesn't matter
 -- I'll have another look at it and remove all the unnecesary imports
 
-  lemma aux1 {f : ℂ → ℂ} {γ : ℝ → ℂ} (hf : DifferentiableAt ℝ (f ∘ γ) x) :
-    DifferentiableAt ℂ f (γ x) := by
-  sorry
+open definitions
 
 open Set
 open Nat Real MeasureTheory Set Filter Function intervalIntegral Interval
-lemma complex_ftc (f : ℂ → ℂ) (γ : ℝ → ℂ)
+lemma aux1 {f : ℂ → ℂ} {γ : ℝ → ℂ} (hf : DifferentiableAt ℝ (f ∘ γ) x) :
+    DifferentiableAt ℂ f (γ x) := by
+  sorry
+
+lemma complex_ftc (f : ℂ → ℂ) (γ : ℝ → ℂ)  
     (hf_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (f ∘ γ) x)
     (hγ_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ γ x)
     (h_int : IntervalIntegrable (deriv (f ∘ γ)) volume 0 1) :
@@ -38,3 +40,32 @@ lemma complex_ftc (f : ℂ → ℂ) (γ : ℝ → ℂ)
     · exact hγ_deriv y hy
   rw [integral_congr this, integral_deriv_eq_sub hf_deriv h_int]
   trivial
+
+lemma aux2 {z w : ℂ} {f : ℂ → ℂ} {γ : Path z w} (hf : DifferentiableAt ℝ (f ∘ (Path.extend γ)) x) :
+           DifferentiableAt ℂ f ((Path.extend γ) x) := by
+  sorry
+
+lemma complex_ftc2 (z w : ℂ) (f : ℂ → ℂ) (γ : Path z w) --  NOT WORKING (YET). THIS USES PATH.EXTEND. NEED TO FIA PROBLEM IN THE LAST REWRITE
+    (hf_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (f ∘ (Path.extend γ)) x)
+    (hγ_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (Path.extend γ) x)
+    (h_int : IntervalIntegrable (deriv (f ∘ (Path.extend γ))) volume 0 1) :
+    pathIntegral1 z w (deriv f) γ = (f ∘ (Path.extend γ)) 1 - (f ∘ (Path.extend γ)) 0 := by
+  unfold pathIntegral1
+  unfold aux
+  simp
+  have : ∀ y ∈ (Set.uIcc 0 1), deriv f ((Path.extend γ) y) * deriv (Path.extend γ) y = deriv (f ∘ (Path.extend γ)) y := by
+    intro y hy
+    rw [deriv.comp]
+    · exact aux2 (hf_deriv y hy)
+    · exact hγ_deriv y hy
+  rw [integral_congr this, integral_deriv_eq_sub hf_deriv h_int]
+  trivial
+
+
+theorem no_name (z w : ℂ) (f : ℂ → ℂ) (γ : Path z w)
+      (hf_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (f ∘ (Path.extend γ)) x)
+      (hγ_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (Path.extend γ) x)
+      (h_int : IntervalIntegrable (deriv (f ∘ (Path.extend γ))) volume 0 1)
+      (hγ_closed : γ 0 = γ 1) :
+      pathIntegral1 z w f γ = 0 := by
+      sorry
