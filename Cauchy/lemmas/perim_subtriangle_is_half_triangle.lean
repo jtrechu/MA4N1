a9a9e782d6091dceed772fcf20ae8b6aed985b33
JoshRydell <58331184+JoshRydell@users.noreply.github.com>
2023-12-02 20:17:25 +0000

    adapting perim_sub_triangles

--- Cauchy/lemmas/perim_subtriangle_is_half_triangle.lean
+++ Cauchy/lemmas/perim_subtriangle_is_half_triangle.lean
@@ -17,14 +17,14 @@ lemma perim_subtriangle_is_half_triangle {T : Triangle}
   (h_perimT_eq_l : perimeter T = l) : perimeter (subTriangle T) = l/2 := by
   unfold perimeter
   unfold perimeter at h_perimT_eq_l
-  have sTa : (subTriangle T).a =  0.5 * (T.b + T.a) := by rfl
-  have sTb : (subTriangle T).b =  0.5 * (T.c + T.b) := by rfl
-  have sTc : (subTriangle T).c =  0.5 * (T.a + T.c) := by rfl
+  have sTa : (subTriangle T).a =  (T.b + T.a)/2 := by rfl
+  have sTb : (subTriangle T).b =  (T.c + T.b)/2 := by rfl
+  have sTc : (subTriangle T).c =   (T.a + T.c)/2 := by rfl
   rw[sTa,sTb,sTc]
   repeat rw[dist_eq_norm]
   repeat rw[dist_eq_norm] at h_perimT_eq_l
 
-  calc ‖0.5 * (T.c + T.b) - 0.5 * (T.b + T.a)‖ + ‖0.5 * (T.a + T.c) - 0.5 * (T.c + T.b)‖ +‖0.5 * (T.b + T.a) - 0.5 * (T.a + T.c)‖ = ‖-1/2 *(T.b-T.a)‖ + ‖-1/2 *(T.c-T.b)‖ + ‖-1/2 *(T.a-T.c)‖  := by ring
+  calc ‖ (T.c + T.b)/2 -  (T.b + T.a)/2‖ + ‖ (T.a + T.c)/2 - (T.c + T.b)/2‖ +‖(T.b + T.a)/2 - (T.a + T.c)/2‖ = ‖-1/2 *(T.b-T.a)‖ + ‖-1/2 *(T.c-T.b)‖ + ‖-1/2 *(T.a-T.c)‖  := by ring
   _ = ‖(-1/2 : ℂ)‖ * ‖T.b - T.a‖ + ‖(-1/2 : ℂ)‖ * ‖T.c - T.b‖ + ‖(-1/2 : ℂ)‖*‖T.a - T.c‖ := by repeat rw[norm_mul]
   _ = ‖(-1/2 : ℂ)‖*(‖T.b - T.a‖ + ‖ T.c - T.b‖ + ‖T.a - T.c‖)  := by ring
   _ = ‖(-1/2 : ℂ)‖*l := by rw[h_perimT_eq_l]
