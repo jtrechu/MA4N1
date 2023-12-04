2d10d0aab397226fc938375f10ee10902714cf14
Edward Watine <02ewatine@gmail.com>
2023-12-04 00:55:16 +0000

    Made some small adjustments to the path file Renamed a couple of files

--- Cauchy/definitions/path.lean
+++ Cauchy/definitions/path.lean
@@ -12,6 +12,7 @@ namespace definitions
 open unitInterval Finset
 structure C1Path extends C(ℝ, ℂ) where
   differentiable_toFun : DifferentiableOn ℝ toFun I
+  continuous_deriv_toFun : ContinuousOn (deriv toFun) I
 
 -- copied from Mathlib Path
 instance C1Path.continuousMapClass : ContinuousMapClass C1Path ℝ ℂ where
@@ -25,5 +26,18 @@ structure PiecewisePath where
   count : ℕ
   paths : Fin count → C1Path
 
+instance : Coe C1Path PiecewisePath where
+  coe := λ p => {count := 1, paths := λ 0 => p}
+
 def constructPiecewisePath (array : Array C1Path) : PiecewisePath :=
-  PiecewisePath.mk array.size λ n => array[n]
+  PiecewisePath.mk array.size λ i => array[i]
+
+def PiecewisePath.extend (path1 path2 : PiecewisePath) : PiecewisePath :=
+  {
+    count := path2.count + path1.count
+    paths := λ i => by
+      by_cases i < path1.count
+      . exact path1.paths $ Fin.castLT i h
+      . simp only [not_lt] at h
+        exact path2.paths $ Fin.subNat path1.count i h
+  }
