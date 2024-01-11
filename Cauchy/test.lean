49ee40d901bf466a05ee02ad4b1f738d0596a59c
Edward Watine <02ewatine@gmail.com>
2024-01-11 17:00:53 +0000

    recursive test!

--- Cauchy/test.lean
+++ Cauchy/test.lean
@@ -1,3 +1,10 @@
 import Mathlib.Tactic
 
-lemma test (f g : ℝ → ℂ) : λ
+noncomputable def test (index : ℕ ) : ℝ := by
+  match index with
+  | 0 => exact 1
+  | n + 1 => exact (test n) / 2
+
+lemma test2 : test 4 = (1/16) := by
+  repeat unfold test
+  ring
