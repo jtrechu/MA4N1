8ad7d46b7aa2d90cdec8cb2ef136a10015163189
juanan27 <148797929+juanan27@users.noreply.github.com>
2024-01-15 12:34:17 +0100

    Update complex_ftc.lean
    forgot to update the complex ftc here!!

--- Cauchy/lemmas/complex_ftc.lean
+++ Cauchy/lemmas/complex_ftc.lean
@@ -29,16 +29,19 @@ lemma aux3 {f : ℂ → ℂ} {γ : C1Path}
     DifferentiableAt ℂ f (γ x) := by
   sorry
 
-lemma complex_ftc {f : ℂ → ℂ} {γ : C1Path}  
+lemma complex_ftc1 {f : ℂ → ℂ} (γ : C1Path)
     (hf_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ (f ∘ γ) x)
     (hγ_deriv : ∀ x ∈ (Set.uIcc 0 1), DifferentiableAt ℝ γ x)
     (h_int : IntervalIntegrable (deriv (f ∘ γ)) volume 0 1) :
-    ∫ y in (0)..(1), deriv f (γ y) * deriv γ y = f (γ 1) - f (γ 0) := by 
+    pathIntegral1' (deriv f) γ = f (γ 1) - f (γ 0) := by
   have : ∀ y ∈ (Set.uIcc 0 1), deriv f (γ y) * deriv γ y = deriv (f ∘ γ) y := by
     intro y hy
     rw [deriv.comp]
     · exact aux3 (hf_deriv y hy)
     · exact hγ_deriv y hy
+  unfold pathIntegral1'
+  unfold aux
+  simp
   rw [integral_congr this, integral_deriv_eq_sub hf_deriv h_int]
   trivial
 
