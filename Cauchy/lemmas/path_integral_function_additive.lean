8cfea29d0862e2833de61e068cbd503e93abaafd
juanan27 <148797929+juanan27@users.noreply.github.com>
2024-01-18 18:32:38 +0100

    Update path_integral_function_additive.lean

--- Cauchy/lemmas/path_integral_function_additive.lean
+++ Cauchy/lemmas/path_integral_function_additive.lean
@@ -29,3 +29,19 @@ lemma pathIntegral_function_additivity {x y : ℂ } (f g : ℂ → ℂ) (γ : Pa
   rw[integral_congr this]
   · exact haux₁
   · exact haux₂
+
+lemma pathIntegral_function_additivity1 (f g : ℂ → ℂ) (γ : C1Path)
+(haux₁: IntervalIntegrable (aux f γ) volume 0 1)
+(haux₂: IntervalIntegrable (aux g γ) volume 0 1) :
+(pathIntegral1' f γ) + (pathIntegral1' g γ) = (pathIntegral1' (f+g) γ) := by
+  unfold pathIntegral1'
+  rw[← intervalIntegral.integral_add]
+  unfold aux
+  have : ∀ x ∈ (Set.uIcc 0 1), (f ∘ γ * deriv γ) x + (g ∘  γ * deriv γ) x = ((f + g) ∘ γ * deriv γ) x := by
+    intro x
+    simp
+    rw[add_mul]
+    aesop
+  rw[integral_congr this]
+  · exact haux₁
+  · exact haux₂
