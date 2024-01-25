3daf4ceee9f613895dbf2ac4b916890375c8abc3
jtrechu <jdtrechuelo@gmail.com>
2024-01-25 13:19:50 +0000

    dslope now has integral 0 with extended cauchy

---
+++ Cauchy/integral_formula/integral_formula_helpers.lean
@@ -0,0 +1,45 @@
+import Mathlib.Analysis.Analytic.Basic
+import Mathlib.Topology.Basic
+import Mathlib.Topology.Connected.Basic
+import Mathlib.Data.Real.Basic
+import Mathlib.Analysis.Complex.ReImTopology
+import Mathlib.Analysis.Calculus.Dslope
+import Mathlib.Data.Complex.Basic
+import Cauchy.definitions.triangle
+import Cauchy.definitions.path_integrals
+import Cauchy.definitions.domain
+import Cauchy.helpers.triangle
+import Cauchy.lemmas.zero_le_of_gt_zero
+import Cauchy.integral_zero._1_triangle_sequence
+import Cauchy.integral_zero._4_integral_bound
+import Cauchy.integral_formula.sorryed_out_lemmas
+
+open definitions lemmas helpers.triangle sorrylemmas
+variable {U : Set ℂ}
+
+lemma UOpen (hU : IsCDomain U) : IsOpen U := by
+  unfold IsCDomain at hU
+  exact hU.1
+
+lemma U_Punctured_Open (hU : IsCDomain U) : IsOpen (U\{z}) := by
+have hz : IsOpen {z}ᶜ  := by exact isOpen_compl_singleton
+apply IsOpen.inter (UOpen hU) hz
+
+lemma trivialU : (U\{z}) ⊆ U := by simp_all only [Set.diff_singleton_subset_iff, Set.subset_insert]
+
+lemma dslope_integral_0 {T : Triangle} {f : ℂ → ℂ} (hU: IsCDomain U)
+(hT : TriangularBoundary T ⊆ U) (z : ℂ) (hfD : DifferentiableOn ℂ f U) ( hz : z ∈ TriangularBoundary T):
+trianglePathIntegral (dslope f z) T = 0 := by
+unfold IsCDomain at hU
+have  hfC : ContinuousOn f U := by exact DifferentiableOn.continuousOn hfD
+have zinU : z ∈ U := by exact hT hz
+have Uopen : IsOpen U := by exact UOpen hU
+have U_nhd : (U ∈ nhds z) := by
+  exact (IsOpen.mem_nhds (Uopen) zinU )
+have hderivAt : DifferentiableAt ℂ  f z := by exact DifferentiableOn.differentiableAt hfD U_nhd
+have gCont : ContinuousOn f U ∧DifferentiableAt ℂ  f z  := by exact ⟨hfC,hderivAt⟩
+rw[←continuousOn_dslope U_nhd] at gCont
+have znotInPuncturedU : z∉(U\{z}) := by simp
+have hgD : DifferentiableOn ℂ f (U\{z}) := by apply DifferentiableOn.mono hfD trivialU
+rw[←differentiableOn_dslope_of_nmem znotInPuncturedU] at hgD
+exact(cauchy_for_triangles_generalised hU hT z gCont hgD)
--- Cauchy/integral_formula/sorryed_out_lemmas.lean
+++ Cauchy/integral_formula/sorryed_out_lemmas.lean
@@ -3,19 +3,22 @@ import Mathlib.Topology.Basic
 import Mathlib.Topology.Connected.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Analysis.Complex.ReImTopology
+import Mathlib.Analysis.Calculus.Dslope
 import Mathlib.Data.Complex.Basic
 import Cauchy.definitions.triangle
 import Cauchy.definitions.path_integrals
 import Cauchy.definitions.domain
 import Cauchy.helpers.triangle
-
 import Cauchy.lemmas.zero_le_of_gt_zero
-
 import Cauchy.integral_zero._1_triangle_sequence
 import Cauchy.integral_zero._4_integral_bound
 
-open definitions lemmas helpers.triangle
+namespace sorrylemmas
 
-theorem cauchy_for_triangles_generalised {U : Set ℂ } {T : Triangle} {f : ℂ  → ℂ }
-(hU: IsCDomain U) (hT : TriangularBoundary T ⊆ U) (z : ℂ) (hf : DifferentiableOn ℂ f (U\{z}))
+open definitions lemmas helpers.triangle
+variable {U : Set ℂ}
+
+theorem cauchy_for_triangles_generalised {T : Triangle} {f : ℂ  → ℂ }
+(hU: IsCDomain U) (hT : TriangularBoundary T ⊆ U) (z : ℂ) (hfC : ContinuousOn f U)
+(hfD : DifferentiableOn ℂ f (U\{z}) )
 : trianglePathIntegral f T = 0 := by sorry
