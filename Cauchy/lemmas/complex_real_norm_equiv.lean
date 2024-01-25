4dc2635c9fca6cd37b02de4bb0441009b67f1a74
Edward Watine <02ewatine@gmail.com>
2024-01-25 12:08:52 +0000

    Remove using

--- Cauchy/lemmas/complex_real_norm_equiv.lean
+++ Cauchy/lemmas/complex_real_norm_equiv.lean
@@ -2,9 +2,6 @@ import Mathlib.Data.Complex.Basic
 import Mathlib.Tactic
 import Mathlib.Algebra.Field.Defs
 
-
-namespace lemmas
-
 --very useful theorem to make Lean behave and not complain
 lemma complex_real_norm_equiv (a b : ℝ) : @norm ℂ Complex.instNormedFieldComplex.toNorm (b - a) = ‖b - a‖ := by
     rewrite [Complex.norm_eq_abs, Complex.abs_def]
